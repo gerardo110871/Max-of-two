1c3ec667ecb7ecfa815579f8e24c8b8a85632ffe
Gerardo Padilla <gpadil@icloud.com>
2024-02-03 19:59:44 -0700

    solved equation

---
+++ maxOfTwo.js
@@ -0,0 +1,14 @@
+// let x = 1
+// let y = 2
+
+// Write your code below this line.
+
+//Max of two numbers
+x = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(Math.max(...x))
+
+x = 1;
+y = 2;
+
+console.log(Math.max(x,y))
